281443003b8dd34f060ea6c25f9d16c7827018aa
dashaluna <dashaluna@gmail.com>
2015-11-06 15:52:31 +0000

    Remove redundant code, as get_post_thumbnail_id() function looks up the current post id by default

--- includes/functions.php
+++ includes/functions.php
@@ -4,8 +4,7 @@
  * @return string Post content, with the simple-fb-caption filter tacked onto it.
  */
 function simple_fb_thumbnail_caption() {
-	$post_id    = get_the_id();
-	$thumb_id   = get_post_thumbnail_id( $post_id );
+	$thumb_id   = get_post_thumbnail_id();
 	$thumb_post = get_post( $thumb_id );
 	$caption    = apply_filters( 'simple-fb-caption', $thumb_post->post_content );
 	return $caption;
