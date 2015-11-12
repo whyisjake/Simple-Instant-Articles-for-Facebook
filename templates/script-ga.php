80bac2b1b238a506cbd43991d2a0d7a02103e332
Matth_eu <matthew@matth.eu>
2015-11-12 22:32:20 +0000

    fix indentation

--- templates/script-ga.php
+++ templates/script-ga.php
@@ -1,6 +1,6 @@
 <figure class="op-tracker">
-    <iframe>
-        <script>
+	<iframe>
+		<script>
 			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -9,5 +9,5 @@
 			ga('create', '<?php echo esc_js( $ga_profile_id ); ?>', 'auto');
 			ga('send', 'pageview');
 		</script>
-    </iframe>
+	</iframe>
 </figure>
