ba75d325a73fcd4d864bd47837f77189fff12ed3
dashaluna <dashaluna@gmail.com>
2015-11-17 16:58:28 +0000

    Add caption param to simple_fb_image_caption() function - so that caption can be specified

--- includes/functions.php
+++ includes/functions.php
@@ -2,28 +2,43 @@
 /**
  * Outputs FB IA formatted caption for an image or a post thumbnail.
  *
- * @param int $image_id Image/attachment ID. Optional.
+ * If caption string is specified - it takes precedence over getting
+ * the caption directly from image/attachment post.
  *
- * @return string|void FB IA formatted image caption.
- *                     Nothing if attachment post isn't found.
+ * @param int    $image_id Image/attachment ID. Optional.
+ * @param string $caption  Image caption string. Optional.
+ *
+ * @return string          FB IA formatted image caption.
+ *                         Empty string if attachment post isn't found.
  */
-function simple_fb_image_caption( $image_id = 0 ) {
+function simple_fb_image_caption( $image_id = 0, $caption = '' ) {
 
-	// Use thumbnail image - if image ID isn't specified.
-	if ( ! $image_id ) {
-		$image_id = get_post_thumbnail_id();
+	$caption_str = '';
+
+	// Specified caption string takes precedence over image ID.
+	if ( $caption ) {
+		$caption_str = $caption;
+
+	} else {
+		// If image ID is not specified - try to get thumbnail ID.
+		if ( ! $image_id = intval( $image_id ) ) {
+			$image_id = get_post_thumbnail_id();
+		}
+
+		// Get attachment post and its caption, aka post excerpt.
+		$attachment_post = get_post( $image_id );
+
+		// Stop if - attachment post not found or caption is empty.
+		if ( ! $attachment_post || ! $attachment_post->post_excerpt ) {
+			return '';
+		}
+
+		$caption_str = $attachment_post->post_excerpt;
 	}
 
-	// Get attachment post and its caption, aka post excerpt.
-	$attachment_post = get_post( $image_id );
-
-	// Stop if - attachment post not found or caption is empty.
-	if ( ! $attachment_post || ! $attachment_post->post_excerpt ) {
-		return;
-	}
-
+	// Output image caption in FB IA format.
 	printf(
 		'<figcaption><h1>%s</h1></figcaption>',
-		esc_html( $attachment_post->post_excerpt )
+		esc_html( $caption_str )
 	);
 }
