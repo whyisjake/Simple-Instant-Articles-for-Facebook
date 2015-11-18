4a0781c508412a0303fcd72340d77fa4f5c47e34
Jake Spurlock <whyisjake@gmail.com>
2015-11-18 12:34:44 -0800

    Just changing around the order a bit.

--- templates/content.php
+++ templates/content.php
@@ -6,10 +6,6 @@
 <article>
 	<header>
 
-		<?php echo simple_fb_header_figure(); ?>
-
-		<?php the_title( '<h1>', '</h1>' ); ?>
-
 		<?php // Kickers aren't really a universal thing on WordPress, so let's just add a filter here for people to hook onto. ?>
 		<?php $kicker = apply_filters( 'simple_fb_kicker', '' ); ?>
 		<?php if ( ! empty( $kicker ) ) : ?>
@@ -18,12 +14,18 @@
 			</details>
 		<?php endif; ?>
 
+		<time class="op-published" dateTime="<?php echo esc_attr( get_post_time( 'Y-m-d\TH:i:s\Z' ) ); ?>"><?php echo esc_html( get_post_time( get_option( 'date_format' ) ) ); ?></time>
+		<time class="op-modified" dateTime="<?php echo esc_attr( get_the_modified_date('Y-m-d\TH:i:s\Z') ); ?>"><?php echo esc_html( get_the_modified_date( get_option( 'date_format' ) ) ); ?></time>
+
+
 		<address>
 			<?php the_author_posts_link(); ?>
 		</address>
 
-		<time class="op-published" dateTime="<?php echo esc_attr( get_post_time( 'Y-m-d\TH:i:s\Z' ) ); ?>"><?php echo esc_html( get_post_time( get_option( 'date_format' ) ) ); ?></time>
-		<time class="op-modified" dateTime="<?php echo esc_attr( get_the_modified_date('Y-m-d\TH:i:s\Z') ); ?>"><?php echo esc_html( get_the_modified_date( get_option( 'date_format' ) ) ); ?></time>
+		<?php echo simple_fb_header_figure(); ?>
+
+		<?php the_title( '<h1>', '</h1>' ); ?>
+
 	</header>
 
 	<?php do_action( 'simple_fb_before_the_content' ); ?>
