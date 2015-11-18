cb747deea96c72ae04a3ef5020cad92679f5a546
Matth_eu <matthew@matth.eu>
2015-11-18 14:44:20 +0000

    don't show cover image if full width media is set

--- templates/article-cover.php
+++ templates/article-cover.php
@@ -16,10 +16,15 @@
 <header>
 
 	<?php
+
+	$has_article_cover = ! empty( get_post_meta( get_the_ID(), '_format_video_embed', true ) );
+	$thumbnail_id      = get_post_thumbnail_id();
+
 	// Post featured image as FB IA cover image.
-	if ( $thumb_id = get_post_thumbnail_id() ) {
-		Simple_FB_Instant_Articles::instance()->render_image_markup( $thumb_id );
+	if ( ! $has_article_cover && $thumbnail_id ) {
+		Simple_FB_Instant_Articles::instance()->render_image_markup( $thumbnail_id );
 	}
+
 	?>
 
 	<?php the_title( '<h1>', '</h1>' ); ?>
