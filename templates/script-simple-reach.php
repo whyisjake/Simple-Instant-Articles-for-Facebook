2efd1db060780b6b1e25c3290c8b39d42c624a18
Matth_eu <matthew@matth.eu>
2015-12-14 16:19:52 +0000

    Ensure simple reach script is loaded

--- templates/script-simple-reach.php
+++ templates/script-simple-reach.php
@@ -1,5 +1,22 @@
+<?php
+
+global $wp_scripts;
+
+if ( ! did_action( 'wp_enqueue_scripts' ) ) {
+	do_action( 'wp_enqueue_scripts' );
+}
+
+// Clone the global $wp_scripts object.
+// Reset it, ready for outputting.
+$_scripts = clone $wp_scripts;
+$_scripts->done = array();
+
+?>
+
 <figure class="op-tracker">
+<h1>Simple Reach </h1>
 	<iframe>
 		<?php lawrence_simple_reach_analytics(); ?>
+		<?php $_scripts->do_items( 'simple-reach-script' ); ?>
 	</iframe>
 </figure>
