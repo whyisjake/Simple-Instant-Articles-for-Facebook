2de1202420778bcb820e854c4a41adea5b8b7c23
Aram Zucker-Scharff <aramzs@salon.com>
2016-02-26 17:34:23 -0500

    Re-add old functions to assure full backwards compatability on Facebook Instant Article templates.

--- simple-facebook-instant-articles.php
+++ simple-facebook-instant-articles.php
@@ -941,5 +941,32 @@ function simple_fb_instant_articles( $file, $version ) {
 	return Simple_FB_Instant_Articles::instance( $file, $version );
 }
 
+
+/**
+ * These functions are included to assure backwards compatability
+ */
+
+
+/**
+ * Generate the caption for a post thumbnail.
+ * @return string Post content, with the simple-fb-caption filter tacked onto it.
+ */
+function simple_fb_thumbnail_caption() {
+	$post_id    = get_the_id();
+	$thumb_id   = get_post_thumbnail_id( $post_id );
+	$thumb_post = get_post( $thumb_id );
+	$caption    = apply_filters( 'simple-fb-caption', $thumb_post->post_excerpt );
+	return $caption;
+}
+/**
+ * Build the header for the story.
+ * @return string header
+ */
+function simple_fb_header_figure() {
+	$caption = sprintf( '<figcaption>%s</figcaption>', simple_fb_thumbnail_caption() );
+	$content = sprintf( '<figure>%s%s</figure>', get_the_post_thumbnail( get_the_id(), 'full' ), $caption );
+	return apply_filters( 'simple_fb_header_figure', $content );
+}
+
 // Kick off the plugin on init.
 simple_fb_instant_articles( __FILE__, '0.5.0' );
